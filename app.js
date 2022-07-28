bd8649459c37d07db917043c131c4304ff2259ea
KristiyanDraganov <kristiandraganov@gmail.com>
2022-07-28 13:43:52 +0300

    colors added

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const container = document.getElementById("container");
-const color = ['', '' ,'', '', ''];
+let color = [];
 const SQUARES = 500;
+
+const btn1 = document.querySelector('.scheme1').addEventListener('click', () => color1());
+const btn2 = document.querySelector('.scheme2').addEventListener('click', () => color2());
+const btn3 = document.querySelector('.scheme3').addEventListener('click', () => color3());
+
+function color1(){
+    color = ['#31302E', '#94908D', '#DAD9D7', '#F0F0F0', 'C3C2BE'];
+}
+
+function color2(){
+    color = ['#EB1D36', '#76BA99', '#00D7FF'];
+}
+
+function color3(){
+    color = ['#0093D3', '#CC016B', '#FFF10D', '#000000'];
+}
 
 for(let i = 0 ; i <SQUARES; i++){
     const square = document.createElement('div');
